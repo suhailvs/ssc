d98297f71a288cfeca6e29a4eb9db9dc6582cf31
suhailv <suhailvs0@gmail.com>
2014-05-24 14:16:23 +0530

    added exam stuffs

--- appexam/urls.py
+++ appexam/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('appexam.views',
+	url(r'^ajax/quest/$','get_question',name='get_question'),
 	url(r'^(?P<tag>.*)/$','startquiz',name='quiz'),
+
 )
--- appexam/views.py
+++ appexam/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from forum.models.tag import Tag
+from forum.models.node import NodeContent
 from forum.models.question import Question
-import random
+import random,json
 from urllib import unquote
+
+from django.http import HttpResponse
+from django.db import connection
 
 # Create your views here.
 def startquiz(request,tag):
@@ -17,6 +21,12 @@ def startquiz(request,tag):
 	d={'questions':quest_pks,'min_left':noQs}
 	return render(request,'exams.html',d)
 
-def exams(request):
-	tg=Tag.objects.all()
-	return render(request,'exams.html',{'tag':tg})
+def get_question(request):
+	#myjs.js -> setQnQuestChoice -> /ajax/quest/
+	query = 'SELECT body FROM forum_node WHERE id = %s' %request.GET['qpk']
+	cursor = connection.cursor()
+	cursor.execute(query)
+	q=cursor.fetchone()
+	n={'question':q[0]}	
+	cursor.close()
+	return HttpResponse(json.dumps(n), mimetype="application/json")
